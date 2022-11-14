cb19265674e15eb635c4e28c757e2f56ec77b9e2
Ting6621 <113186567+Ting6621@users.noreply.github.com>
2022-11-14 20:01:10 +0800

    Update show_file_names.c

--- 实验代码/实验6/print_file/show_file_names.c
+++ 实验代码/实验6/print_file/show_file_names.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
-#include "show_file_names.h"
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
 
+void show_file_names(char[]);
 
-void show_file_names(char* path){
-    printf("printf all of the dir's file-names\n");
-}
-
-
-
-int main(int argc, char const *argv[])
+int main(int argc, char** argv)
 {
-    char path[] = "./";
-    show_file_names(path);
-
-
-    return 0;
+    if(argc == 1){//查看当前目录
+        show_file_names(".");
+    }else{
+        while(--argc){//查看指定的一个或多个目录
+            printf("%s : \n", * ++argv);//打印当前所查看的目录路径
+            show_file_names(*argv);
+        }
+    }
 }
+
+void show_file_names(char dirname[])
+{
+    DIR *dir_ptr;//文件夹指针
+    struct dirent *direntp;//存放文件夹目录内容
+
+    if((dir_ptr = opendir(dirname)) == NULL){//open环节
+        fprintf(stderr, "ls1: open error %s\n",dirname);
+    }else{
+        while((direntp = readdir(dir_ptr)) != NULL)
+            printf("%s\n",direntp -> d_name);
+        }
+        int flag;
+        if(( flag = closedir(dir_ptr)) == -1){//close环节
+            perror("ls1: close dir error");
+            exit(0);
+        }
+ }
+}
+
